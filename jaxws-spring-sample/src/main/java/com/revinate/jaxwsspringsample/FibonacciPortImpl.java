a78cd85d91ed2b0e6af62862f6c24e4a60ff0bf0
Jeremy Landis <jeremylandis@hotmail.com>
2026-01-11 18:21:14 -0500

    Refactor how LICENSE is applied along with it being automated
    The longer license entries in the core files now just reference to the license splits listed directly on the project.

--- jaxws-spring-sample/src/main/java/com/revinate/jaxwsspringsample/FibonacciPortImpl.java
+++ jaxws-spring-sample/src/main/java/com/revinate/jaxwsspringsample/FibonacciPortImpl.java
@@ -1,3 +1,10 @@
+/*
+ * This project is dual-licensed under the terms of the Common Development and Distribution License (CDDL) v1.0 and the GNU General Public License (GPL) v2.0 with Classpath Exception.
+ *
+ * You may choose either license.
+ *
+ * See LICENSE-CDDL-1.0 and LICENSE-GPL-2.0-CE for full license texts.
+ */
 package com.revinate.jaxwsspringsample;
 
 import com.revinate.sample.service.FibonacciFault;
